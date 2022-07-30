6ac43204ab12e191f0a30ad4a9774cb5ae9d0619
Rustem <r132420@gmail.com>
2022-07-30 12:29:45 +0300

    Создал portfolio

---
+++ lib/pages/home/components/header.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:portfolio/models/header_item.dart';
+import 'package:portfolio/utils/constants.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+
+List<HeaderItem> headerItems = [
+  HeaderItem(title: "HOME", onTap: () {}),
+  HeaderItem(title: "MY INTRO", onTap: () {}),
+  HeaderItem(title: "SERVICES", onTap: () {}),
+  HeaderItem(title: "PORTFOLIO", onTap: () {}),
+  HeaderItem(title: "TESTIMONIALS", onTap: () {}),
+  HeaderItem(title: "BLOGS", onTap: () {}),
+  HeaderItem(
+    title: "HIRE ME",
+    onTap: () {},
+    isButton: true,
+  ),
+];
+
+class HeaderLogo extends StatelessWidget {
+  const HeaderLogo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: GestureDetector(
+          onTap: () {},
+          child: RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: "M",
+                  style: GoogleFonts.oswald(
+                    color: Colors.white,
+                    fontSize: 32.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                TextSpan(
+                  text: ".",
+                  style: GoogleFonts.oswald(
+                    color: kPrimaryColor,
+                    fontSize: 36.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class HeaderRow extends StatelessWidget {
+  const HeaderRow({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveVisibility(
+      visible: false,
+      visibleWhen: [
+        Condition.largerThan(name: "MOBILE"),
+      ],
+      child: Row(
+        children: headerItems
+            .map((item) => item.isButton
+                ? MouseRegion(
+                    cursor: SystemMouseCursors.click,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: kDangerColor,
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: TextButton(
+                        child: Text(
+                          item.title,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 13.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        onPressed: item.onTap,
+                      ),
+                    ),
+                  )
+                : MouseRegion(
+                    cursor: SystemMouseCursors.click,
+                    child: Container(
+                      margin: EdgeInsets.only(right: 30.0),
+                      child: GestureDetector(
+                        onTap: item.onTap,
+                        child: Text(
+                          item.title,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 13.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ))
+            .toList(),
+      ),
+    );
+  }
+}
+
+class Header extends StatelessWidget {
+  const Header({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          HeaderLogo(),
+        ],
+      ),
+    );
+  }
+}
